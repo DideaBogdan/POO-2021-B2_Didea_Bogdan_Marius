f9c4c23db8996cc8d2a9bbcfe64c345cac441566
DideaBogdan <79209391+DideaBogdan@users.noreply.github.com>
2021-03-02 20:06:32 +0200

    Lab1-p1 solved

--- Lab2/Lab1-p1/Lab1-p1.cpp
+++ Lab2/Lab1-p1/Lab1-p1.cpp
@@ -4,12 +4,38 @@ int main()
 {
     NumberList nl;
     nl.Init();
-    int x;
-    do
+    int n, x, a;
+    int full = 0;
+    cout << "MENIU \n0.END \n1.Add n numbers. \n2.Sort. \n3.Print. \n";
+    cin >> x;
+    while (x != 0)
     {
+        switch (x)
+        {
+        case 0:
+            break;
+        case 1:
+            cout << "Insert how many numbers you want to add: ";
+            cin >> n;
+            cout << "Insert the numbers: ";
+            for (int i = 1; i <= n ; i++)
+            {
+                cin >> a;
+                if(full==0)
+                    if (nl.Add(a) == false) full = 1;
+            }
+            break;
+        case 2:
+            nl.Sort();
+            cout << "\nThe numbers have been sorted. \n";
+            break;
+        case 3:
+            cout << "\nThe vector is: ";
+            nl.Print();
+            cout << "\n";
+            break;
+        }
         cin >> x;
-    } while (nl.Add(x) == true);
-    nl.Sort();
-    nl.Print();
+    }
     return 0;
  }
--- Lab2/Lab1-p1/NumberList.cpp
+++ Lab2/Lab1-p1/NumberList.cpp
@@ -7,17 +7,17 @@ void NumberList::Init()
 
 bool NumberList::Add(int x)
 {
-	count = count + 1;
-	numbers[count] = x;
-	if (count >= 10) return false;
-	else return true;
+	if (count >= 10) return false; 
+	numbers[count++] = x;
+	return true;
+
 }
 
 void NumberList::Sort()
 {
 	int i, j, aux;
-	for(i = 1; i < count; i++)
-		for(j = i+1; j <= count; j++)
+	for(i = 0; i < count-1; i++)
+		for(j = i+1; j < count; j++)
 			if (numbers[i] >= numbers[j])
 			{
 				aux = numbers[i];
@@ -28,6 +28,6 @@ void NumberList::Sort()
 
 void NumberList::Print()
 {
-	for (int i = 1; i <= count; i++)
+	for (int i = 0; i < count; i++)
 		cout << numbers[i] << " ";
 }
